e1cf2217342867caa707484b2fbe7dd516b975fd
rmp22 <195054967+rmp22@users.noreply.github.com>
2024-06-17 19:55:36 +0900

    extra: add blur support
    Change-Id: I5fabdbbdfe812346f17dbba5d4bf9abe7344e0ca

--- product.mk
+++ product.mk
@@ -9,6 +9,20 @@ EXTRA_PATH := vendor/extra
 # Bootanimation
 include $(EXTRA_PATH)/config/bootanimation.mk
 
+# Enable blur
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true \
+    persist.sysui.disableBlur=false \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false \
+    persist.sysui.disableBlur=true \
+    ro.surface_flinger.supports_background_blur=0
+endif
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkOverlayLumine
